6c7b64c943a856967beb1c98a22937dc14efb737
Kramer Fabian <komplexmaster@googlemail.com>
2009-09-17 11:38:41 +0200

    deleted:    debug/FlCompact.log 	modified:   main.cpp 	deleted:    release/FlCompact.log 	deleted:    release/LogViewer.exe 	deleted:    release/QtCore4.dll 	deleted:    release/QtCored4.dll 	deleted:    release/QtGui4.dll 	deleted:    release/QtGuid4.dll 	deleted:    release/QtNetwork4.dll 	deleted:    release/QtWebKit4.dll 	deleted:    release/QtWebKitd4.dll 	deleted:    release/QtXml4.dll 	deleted:    release/QtXmlPatterns4.dll 	deleted:    release/QtXmlPatternsd4.dll 	deleted:    release/QtXmld4.dll 	deleted:    release/logdelegate.o 	deleted:    release/logfile.o 	deleted:    release/logfilefilter.o 	deleted:    release/logfilemodel.o 	deleted:    release/logfileproxymodel.o 	deleted:    release/logfileviewwidget.o 	deleted:    release/logfileviewwidgetindexbar.o 	deleted:    release/logitem.o 	deleted:    release/logviewdockwidget.o 	deleted:    release/main.o 	deleted:    release/mainwindow.o 	deleted:    release/moc_logfilemodel.cpp 	deleted:    release/moc_logfilemodel.o 	deleted:    release/moc_logfileviewwidget.cpp 	deleted:    release/moc_logfileviewwidget.o 	deleted:    release/moc_logviewdockwidget.cpp 	deleted:    release/moc_logviewdockwidget.o 	deleted:    release/moc_mainwindow.cpp 	deleted:    release/moc_mainwindow.o 	deleted:    release/qrc_res.cpp 	deleted:    release/qrc_res.o

--- main.cpp
+++ main.cpp
@@ -11,21 +11,7 @@ static int test = 0;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QPixmap pixmap(":/img/splash");
-
-    qDebug() << test;
-
-    test++;
-
-    QSplashScreen splash(pixmap);
-
     MainWindow w;
-
     w.show();
-    //splash.show();
-
-    splash.showMessage("Loaded modules"+QString::number(test));
-
     return a.exec();
 }
